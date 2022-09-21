65d225ccfa7b8410cb93bd2d3f0882fe42c82df0
Artem <baryshnartem1993@gmail.com>
2022-09-21 15:59:37 +0300

    asterix

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '')
+// const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '')
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
+
+// const a = prompt('Один из последних просмотреных фильмов?', ''),
+//       b = prompt('На сколько оцените его?',''),
+//       c = prompt('Один из последних просмотреных фильмов?', ''),
+//       d = prompt('На сколько оцените его?','')
+
+// movies[a] = personalMovieDB[b]
+// movies[c] = personalMovieDB[d]
+
+
+let result = ""
+const length = 7
+
+for (let i = 1; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    }
+    result += '\n';
 }
 
-const a = prompt('Один из последних просмотреных фильмов?', ''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотреных фильмов?', ''),
-      d = prompt('На сколько оцените его?','')
-
-movies[a] = personalMovieDB[b]
-movies[c] = personalMovieDB[d]
-
-
+console.log(result)
